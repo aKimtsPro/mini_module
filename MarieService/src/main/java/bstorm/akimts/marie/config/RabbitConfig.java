a77b3cb0bb6db282e2ef3ea439ed79a5bdaa9246
Raphaël Jungers <raphael.jungers@bstorm.be>
2022-04-26 14:28:07 +0200

    Fix connection

--- MarieService/src/main/java/bstorm/akimts/marie/config/RabbitConfig.java
+++ MarieService/src/main/java/bstorm/akimts/marie/config/RabbitConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,18 +15,27 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     @Bean("publish")
-    public Queue marieQueue(@Value("${rabbitmq.publish-to}")String queueName){
+    public Queue marieQueue(@Value("${rabbitmq.publish-to}") String queueName) {
         return new Queue(queueName, true);
     }
 
     @Bean
-    public TopicExchange MessageExchange(@Value("${rabbitmq.exchange-name}") String exchangeName){
+    public Queue lucQueue(@Value("${rabbitmq.listen-to}") String queueName) {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange MessageExchange(@Value("${rabbitmq.exchange-name}") String exchangeName) {
         return new TopicExchange(exchangeName);
     }
 
     @Bean
-    public Binding binding(Queue q, TopicExchange e){
+    public Binding binding(@Qualifier("publish") Queue q, TopicExchange e) {
         return BindingBuilder.bind(q).to(e).with("marie");
     }
 
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
 }
